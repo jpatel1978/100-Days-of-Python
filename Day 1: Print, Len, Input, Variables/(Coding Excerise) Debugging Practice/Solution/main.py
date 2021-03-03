948f92371c3f55e7f588b8c48214824c493fec5b
Hridaya <hridayahoney@gmail.com>
2021-03-03 16:46:03 +0530

    Create main.py

---
+++ Day 1: Print, Len, Input, Variables/(Coding Excerise) Debugging Practice/Solution/main.py
@@ -0,0 +1,4 @@
+print("Day 1 - Lecture 1.6 - String Manuplation & Indentation")
+print("String Concatenation is done with the '+' sign. Intdentation is most important in Python!")
+print('Example: print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
