6505acfd015e7656ef3b714928eaea6775c5cbcf
Hridaya Agrawal <hridayahoney@gmail.com>
2021-04-11 09:39:40 +0530

    Create main.py

---
+++ Day 2: Data Types, Mathematical Operations/(Coding Excerise) Life In Weeks/main.py
@@ -0,0 +1,36 @@
+
+welcome = """
+░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░
+░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗
+░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║
+░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║
+░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝
+░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░
+"""
+
+logo = """
+
+                                                                                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                                                                     
+YYYYYYY       YYYYYYY                                                        LLLLLLLLLLL               iiii     ffffffffffffffff                         iiii                        WWWWWWWW                           WWWWWWWW                                   kkkkkkkk                          
+Y:::::Y       Y:::::Y                                                        L:::::::::L              i::::i   f::::::::::::::::f                       i::::i                       W::::::W                           W::::::W                                   k::::::k                          
+Y:::::Y       Y:::::Y                                                        L:::::::::L               iiii   f::::::::::::::::::f                       iiii                        W::::::W                           W::::::W                                   k::::::k                          
+Y::::::Y     Y::::::Y                                                        LL:::::::LL                      f::::::fffffff:::::f                                                   W::::::W                           W::::::W                                   k::::::k                          
+YYY:::::Y   Y:::::YYYooooooooooo   uuuuuu    uuuuuu rrrrr   rrrrrrrrr          L:::::L               iiiiiii  f:::::f       ffffffeeeeeeeeeeee         iiiiiiinnnn  nnnnnnnn          W:::::W           WWWWW           W:::::W eeeeeeeeeeee        eeeeeeeeeeee    k:::::k    kkkkkkk  ssssssssss   
+   Y:::::Y Y:::::Y oo:::::::::::oo u::::u    u::::u r::::rrr:::::::::r         L:::::L               i:::::i  f:::::f           ee::::::::::::ee       i:::::in:::nn::::::::nn         W:::::W         W:::::W         W:::::Wee::::::::::::ee    ee::::::::::::ee  k:::::k   k:::::k ss::::::::::s  
+    Y:::::Y:::::Y o:::::::::::::::ou::::u    u::::u r:::::::::::::::::r        L:::::L                i::::i f:::::::ffffff    e::::::eeeee:::::ee      i::::in::::::::::::::nn         W:::::W       W:::::::W       W:::::We::::::eeeee:::::ee e::::::eeeee:::::eek:::::k  k:::::kss:::::::::::::s 
+     Y:::::::::Y  o:::::ooooo:::::ou::::u    u::::u rr::::::rrrrr::::::r       L:::::L                i::::i f::::::::::::f   e::::::e     e:::::e      i::::inn:::::::::::::::n         W:::::W     W:::::::::W     W:::::We::::::e     e:::::ee::::::e     e:::::ek:::::k k:::::k s::::::ssss:::::s
+      Y:::::::Y   o::::o     o::::ou::::u    u::::u  r:::::r     r:::::r       L:::::L                i::::i f::::::::::::f   e:::::::eeeee::::::e      i::::i  n:::::nnnn:::::n          W:::::W   W:::::W:::::W   W:::::W e:::::::eeeee::::::ee:::::::eeeee::::::ek::::::k:::::k   s:::::s  ssssss 
+       Y:::::Y    o::::o     o::::ou::::u    u::::u  r:::::r     rrrrrrr       L:::::L                i::::i f:::::::ffffff   e:::::::::::::::::e       i::::i  n::::n    n::::n           W:::::W W:::::W W:::::W W:::::W  e:::::::::::::::::e e:::::::::::::::::e k:::::::::::k      s::::::s      
+       Y:::::Y    o::::o     o::::ou::::u    u::::u  r:::::r                   L:::::L                i::::i  f:::::f         e::::::eeeeeeeeeee        i::::i  n::::n    n::::n            W:::::W:::::W   W:::::W:::::W   e::::::eeeeeeeeeee  e::::::eeeeeeeeeee  k:::::::::::k         s::::::s   
+       Y:::::Y    o::::o     o::::ou:::::uuuu:::::u  r:::::r                   L:::::L         LLLLLL i::::i  f:::::f         e:::::::e                 i::::i  n::::n    n::::n             W:::::::::W     W:::::::::W    e:::::::e           e:::::::e           k::::::k:::::k  ssssss   s:::::s 
+       Y:::::Y    o:::::ooooo:::::ou:::::::::::::::uur:::::r                 LL:::::::LLLLLLLLL:::::Li::::::if:::::::f        e::::::::e               i::::::i n::::n    n::::n              W:::::::W       W:::::::W     e::::::::e          e::::::::e         k::::::k k:::::k s:::::ssss::::::s
+    YYYY:::::YYYY o:::::::::::::::o u:::::::::::::::ur:::::r                 L::::::::::::::::::::::Li::::::if:::::::f         e::::::::eeeeeeee       i::::::i n::::n    n::::n               W:::::W         W:::::W       e::::::::eeeeeeee   e::::::::eeeeeeee k::::::k  k:::::ks::::::::::::::s 
+    Y:::::::::::Y  oo:::::::::::oo   uu::::::::uu:::ur:::::r                 L::::::::::::::::::::::Li::::::if:::::::f          ee:::::::::::::e       i::::::i n::::n    n::::n                W:::W           W:::W         ee:::::::::::::e    ee:::::::::::::e k::::::k   k:::::ks:::::::::::ss  
+    YYYYYYYYYYYYY    ooooooooooo       uuuuuuuu  uuuurrrrrrr                 LLLLLLLLLLLLLLLLLLLLLLLLiiiiiiiifffffffff            eeeeeeeeeeeeee       iiiiiiii nnnnnn    nnnnnn                 WWW             WWW            eeeeeeeeeeeeee      eeeeeeeeeeeeee kkkkkkkk    kkkkkkksssssssssss    
+                                                                                                                                                                                                                                                                                             
+"""
+
+print(welcome)
+print(logo)
+age = input("What is your current age?")
