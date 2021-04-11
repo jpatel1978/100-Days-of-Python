0fa82895d65010ae46a42ea8283655661fa19190
Hridaya Agrawal <hridayahoney@gmail.com>
2021-04-11 20:30:44 +0530

    Create main.py

---
+++ Day 4: Randomization & Python Lists/(Coding Excerise) Quiz/main.py
@@ -0,0 +1,46 @@
+import random
+
+  
+welcome = """
+░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░
+░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗
+░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║
+░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║
+░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝
+░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░
+"""
+
+logo = """
+                                                                                                        
+                                                                                                        
+     QQQQQQQQQ                         iiii                                      iiii                   
+   QQ:::::::::QQ                      i::::i                                    i::::i                  
+ QQ:::::::::::::QQ                     iiii                                      iiii                   
+Q:::::::QQQ:::::::Q                                                                                     
+Q::::::O   Q::::::Quuuuuu    uuuuuu  iiiiiii zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzziiiiiii zzzzzzzzzzzzzzzzz
+Q:::::O     Q:::::Qu::::u    u::::u  i:::::i z:::::::::::::::zz:::::::::::::::zi:::::i z:::::::::::::::z
+Q:::::O     Q:::::Qu::::u    u::::u   i::::i z::::::::::::::z z::::::::::::::z  i::::i z::::::::::::::z 
+Q:::::O     Q:::::Qu::::u    u::::u   i::::i zzzzzzzz::::::z  zzzzzzzz::::::z   i::::i zzzzzzzz::::::z  
+Q:::::O     Q:::::Qu::::u    u::::u   i::::i       z::::::z         z::::::z    i::::i       z::::::z   
+Q:::::O     Q:::::Qu::::u    u::::u   i::::i      z::::::z         z::::::z     i::::i      z::::::z    
+Q:::::O  QQQQ:::::Qu::::u    u::::u   i::::i     z::::::z         z::::::z      i::::i     z::::::z     
+Q::::::O Q::::::::Qu:::::uuuu:::::u   i::::i    z::::::z         z::::::z       i::::i    z::::::z      
+Q:::::::QQ::::::::Qu:::::::::::::::uui::::::i  z::::::zzzzzzzz  z::::::zzzzzzzzi::::::i  z::::::zzzzzzzz
+ QQ::::::::::::::Q  u:::::::::::::::ui::::::i z::::::::::::::z z::::::::::::::zi::::::i z::::::::::::::z
+   QQ:::::::::::Q    uu::::::::uu:::ui::::::iz:::::::::::::::zz:::::::::::::::zi::::::iz:::::::::::::::z
+     QQQQQQQQ::::QQ    uuuuuuuu  uuuuiiiiiiiizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzziiiiiiiizzzzzzzzzzzzzzzzz
+             Q:::::Q                                                                                    
+              QQQQQQ                                                                                    
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+"""
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+print(welcome)
+print(logo)
+questions=["Who is the Founder of Microsoft?", "What is the full form of HTML?"]
+answers=[["Bill Gates", "Learn Earn & Fun", "Albert Einstein"], ["Hyper Text Markup Language", "How Text Mood Lays",
+                                                                 "Hi Tomato Mango Lingo"]]
+correct_answer  = ["Bill Gates", "Hyper Text Markup Language"]
